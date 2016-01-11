11769619944f9055c99f1a0669202bb5b0cff00b
Sarah Yu <sarah.m.yu88@gmail.com>
2016-01-11 09:42:23 -0800

    Add better root url response

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(responseTime());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', function(req,res){res.send('Working updated again.');});
+app.get('/', function(req,res){res.send('Please use proper endpoint.');});
 app.get('/radio/keywords', wordpressHandler.keywords);
 app.get('/radio/keywords/perspectives', wordpressHandler.perspectives);
 app.get('/radio/programs', wordpressHandler.programs);

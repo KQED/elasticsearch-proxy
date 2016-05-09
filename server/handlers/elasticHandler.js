c01ec8f964d20ba43596f9241e1714e999ae780d
Sarah Yu <sarah.m.yu88@gmail.com>
2016-05-09 09:34:55 -0700

    Add middleware to prevent posts from non-indexed sites and staging to be indexed and fixed bug with multiple ip headers

--- server/handlers/elasticHandler.js
+++ server/handlers/elasticHandler.js
@@ -5,10 +5,6 @@ var requestUtil = require('../utils/requestUtil'),
 module.exports = {
 
   addWordpressDocument: function(req, res) {
-    var link = req.body.link;
-    
-    //Prevent posts from staging to be added to elasticsearch
-    if(link.match('ww2.staging.wpengine') === null) {
   
       log.info('Attempting to add entry: ' + JSON.stringify(req.body));
       
@@ -17,46 +13,23 @@ module.exports = {
       var endpoint = processData.processEndpoint(req.body) + req.body.site_id + '%24' + entry.id;
 
       requestUtil.handleElasticEntries(entry, endpoint, 'PUT', res);
-      
-    } else {
-      
-      log.info('Entry is from staging');
-
-      res.status(200).send('Cannot add entry from staging');
-
-    }
-  
+        
   },
 
   removeWordpressDocument: function(req, res) {
 
-    var link = req.body.link;
-    
-    if(link.match('ww2.staging.wpengine') === null) {
 
       log.info('Attempting to remove entry: ' + JSON.stringify(req.body));
         
-        var entry = processData.processPost(req.body);
-  
-        var endpoint = processData.processEndpoint(req.body) + req.body.site_id + '%24' + entry.id;
+      var entry = processData.processPost(req.body);
+
+      var endpoint = processData.processEndpoint(req.body) + req.body.site_id + '%24' + entry.id;
   
       requestUtil.handleElasticEntries(entry, endpoint, 'DELETE', res);
-
-    } else {
-
-      log.info('Entry is from staging');
-
-      res.status(200).send('Cannot add entry from staging');
-    
-    }
 
   },
   
   updateWordpressDocument: function(req, res) {
-
-    var link = req.body.link;
-    
-    if(link.match('ww2.staging.wpengine') === null) {
 
       log.info('Attempting to update entry: ' + JSON.stringify(entry));
       
@@ -69,12 +42,7 @@ module.exports = {
         var endpoint = processData.processEndpoint(req.body) + req.body.site_id + '%24' + req.body.id + '/_update';
   
         requestUtil.handleElasticEntries(entry, endpoint, 'POST', res);
-    } else {
-      log.info('Entry is from staging');
-
-      res.status(200).send('Cannot add entry from staging');
-    }
-  
+          
   }
 
 };

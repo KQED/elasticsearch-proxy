144f886fd34b429e3ba052a886da4e155d11aac0
Sarah Yu <sarah.m.yu88@gmail.com>
2016-06-29 09:42:20 -0700

    Add Math.floor in case utc timesetting ever changes

--- server/utils/timeUtils.js
+++ server/utils/timeUtils.js
@@ -4,10 +4,10 @@ module.exports = {
     
     if(dateType === 'end') {
       unixDate.setUTCHours(24,0,0,0);
-      return unixDate.getTime() / 1000;      
+      return Math.floor(unixDate.getTime() / 1000);      
     }
     
     unixDate.setUTCHours(0,0,0,0);
-    return unixDate.getTime() / 1000;      
+    return Math.floor(unixDate.getTime() / 1000);     
   }
 };

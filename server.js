36b8c22ce2bec59f4fb3d5920c6e3dab121d4ab9
Cynthia Chen <cynthiaechen@gmail.com>
2016-12-20 10:37:43 -0800

    Add routes and handlers for retrieving chronological entries and search by keywords or dates from variable sites.

--- server.js
+++ server.js
@@ -8,9 +8,10 @@ var express = require('express'),
     filterMiddleware = require('./server/utils/filterMiddleware'),
     PORT = process.env.PORT || 8080,
     log = require('./server/logging/bunyan'),
-    wordpressHandler = require('./server/handlers/wordpressRadioHandler'),
-    wordpressPerspectivesHandler = require('./server/handlers/wordpressPerspectivesHandler'),
+    wordpressRadioHandler = require('./server/handlers/wordpressRadioHandler'),
     wordpressPostsHandler = require('./server/handlers/wordpressPostsHandler'),
+    wordpressPerspectivesHandler = require('./server/handlers/wordpressPerspectivesHandler'),
+    wordpressElectionHandler = require('./server/handlers/wordpressElectionHandler'),
     wordpressForumHandler = require('./server/handlers/wordpressForumHandler'),
     elasticHandler = require('./server/handlers/elasticHandler');
 
@@ -27,24 +28,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', function(req,res){res.send('Please use proper endpoint.');});
-app.get('/radio/keywords', wordpressHandler.keywords);
+app.get('/radio/keywords', wordpressRadioHandler.keywords);
 app.get('/radio/keywords/perspectives', wordpressPerspectivesHandler.keywords);
 app.get('/radio/keywords/forum', wordpressForumHandler.keywords);
-app.get('/radio/programs', wordpressHandler.programs);
-app.get('/radio/dates', wordpressHandler.dates);
+app.get('/radio/programs', wordpressRadioHandler.programs);
+app.get('/radio/dates', wordpressRadioHandler.dates);
 app.get('/radio/dates/perspectives', wordpressPerspectivesHandler.dates);
 app.get('/radio/dates/forum', wordpressForumHandler.dates);
 
-app.get('/wordpress', wordpressPostsHandler.chronological);
-// app.get('/elections/featured', wordpressPostsHandler.featuredPost);
+// app.get('/wordpress', wordpressElectionHandler.chronological);
+app.get('/wordpress/:site', wordpressPostsHandler.chronological);
+app.get('/wordpress/keywords/:site', wordpressPostsHandler.keywords);
+app.get('/wordpress/dates/:site', wordpressPostsHandler.dates);
 
 app.post('/radio/posts', filterMiddleware.ipFilter, filterMiddleware.postFilter, elasticHandler.addWordpressDocument);
 app.delete('/radio/posts', filterMiddleware.ipFilter, filterMiddleware.postFilter, elasticHandler.removeWordpressDocument);
 app.put('/radio/posts', filterMiddleware.ipFilter, filterMiddleware.postFilter, elasticHandler.updateWordpressDocument);
 
-app.post('/wordpress/posts', filterMiddleware.ipFilter, filterMiddleware.electionsFilter, elasticHandler.addWordpressDocument);
-app.delete('/wordpress/posts', filterMiddleware.ipFilter, filterMiddleware.electionsFilter, elasticHandler.removeWordpressDocument);
-app.put('/wordpress/posts', filterMiddleware.ipFilter, filterMiddleware.electionsFilter, elasticHandler.updateWordpressDocument);
+app.post('/wordpress/posts', filterMiddleware.ipFilter, filterMiddleware.postFilter, elasticHandler.addWordpressDocument);
+app.delete('/wordpress/posts', filterMiddleware.ipFilter, filterMiddleware.postFilter, elasticHandler.removeWordpressDocument);
+app.put('/wordpress/posts', filterMiddleware.ipFilter, filterMiddleware.postFilter, elasticHandler.updateWordpressDocument);
 
 var server = app.listen(PORT, function(){
   log.info('Server listening on port ' + PORT);

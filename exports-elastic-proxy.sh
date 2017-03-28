f0f3f25c7d641063bded3ece18811fd77f7970ec
Tushar  Borole <tusharborole@ymail.com>
2017-03-28 11:46:57 -0700

    DIG-143 TCR AA Elasticsearch filter by tag for the News site
    DIG-143 TCR AA Elasticsearch filter by tag for the News site
    
    DIG-143 TCR AA Elasticsearch filter by tag for the News site

--- exports-elastic-proxy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# export ELASTIC='http://search-kqedsearch-wgdmvkk5xnkkumnaxvjgraywmy.us-west-1.es.amazonaws.com/'
-export ELASTIC='https://search-kqed-bcoimf4qu5b22h27ud4eqcz52a.us-west-1.es.amazonaws.com/'
-export INDEX='/wptest/'
-export IP1='127.0.0.1'

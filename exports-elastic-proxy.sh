4519138576049a9a0f028733dfb58cc6da9f40d8
Tushar  Borole <tusharborole@ymail.com>
2017-03-28 11:46:57 -0700

    # This is a combination of 2 commits. # The first commit's message is:
    DIG-143 TCR AA Elasticsearch filter by tag for the News site
    
    # This is the 2nd commit message:
    
    DIG-143 TCR AA Elasticsearch filter by tag for the News site

---
+++ exports-elastic-proxy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# export ELASTIC='http://search-kqedsearch-wgdmvkk5xnkkumnaxvjgraywmy.us-west-1.es.amazonaws.com/'
+export ELASTIC='https://search-kqed-bcoimf4qu5b22h27ud4eqcz52a.us-west-1.es.amazonaws.com/'
+export INDEX='/wptest/'
+export IP1='127.0.0.1'
---
+++ exports2.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export ELASTIC='https://search-kqed-bcoimf4qu5b22h27ud4eqcz52a.us-west-1.es.amazonaws.com/'
+export WORDPRESS='http://ww2.kqed.org/'
+export INDEX='/wptest/'
+export SNAPSHOT_ENDPOINT='/_snapshot/kqed-backups/snapshot_'
